f9795f02722ad0fa00731835b4fcaea2bf4c1add
Natalia Gierszewska <ngiersz@gmail.com>
2022-02-21 22:47:13 +0100

    add copy to main page

--- app/src/main/java/com/example/youtubetto/MainActivity.kt
+++ app/src/main/java/com/example/youtubetto/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.text)
 
-        val listView : ListView = findViewById(R.id.videos_list);
+//        val listView : ListView = findViewById(R.id.videos_list);
 
         val queue = Volley.newRequestQueue(this)
         val url = "https://www.googleapis.com/youtube/v3/playlists?key=${youtube_api_key}&channelId=${channel_id}&part=snippet"
@@ -33,60 +33,53 @@ class MainActivity : AppCompatActivity() {
             val signInIntent = GoogleSignInActivity.newIntent(this)
             startActivity(signInIntent)
         }
-
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                val items : JSONArray = response.getJSONArray("items")
-
-                val playlistId : String = items.getJSONObject(0).getString("id")
-                val playlistTitle : String = items.getJSONObject(0).getJSONObject("snippet").getJSONObject("localized").getString("title")
-                val channelTitle : String = items.getJSONObject(0).getJSONObject("snippet").getString("channelTitle")
-                textView.text = "Filmy z playlisty: " + playlistTitle + ", kanał: " + channelTitle
-
-                val jsonObjectRequestVideos = JsonObjectRequest(
-                    Request.Method.GET, "https://www.googleapis.com/youtube/v3/playlistItems?key=${youtube_api_key}&playlistId=${playlistId}&part=snippet", null,
-                    { response ->
-                        val items : JSONArray = response.getJSONArray("items")
-
-//                        val videoTitles = ArrayList<String>()
-                        val videoTitles = arrayOfNulls<String>(items.length())
-//                        for (i in videoTitles) {
-//                            val snippet : JSONObject = items.getJSONObject(i).getJSONObject("snippet")
+//                to wszystko zakomentowane - pobieranie filmów z playlisty, wyświetlenie tytułów na liście, przejście do video view po kliknięciu
+//        val jsonObjectRequest = JsonObjectRequest(
+//            Request.Method.GET, url, null,
+//            { response ->
+//                val items : JSONArray = response.getJSONArray("items")
 //
+//                val playlistId : String = items.getJSONObject(0).getString("id")
+//                val playlistTitle : String = items.getJSONObject(0).getJSONObject("snippet").getJSONObject("localized").getString("title")
+//                val channelTitle : String = items.getJSONObject(0).getJSONObject("snippet").getString("channelTitle")
+
+
+
+
+//                textView.text = "Filmy z playlisty: " + playlistTitle + ", kanał: " + channelTitle
+//                val jsonObjectRequestVideos = JsonObjectRequest(
+//                    Request.Method.GET, "https://www.googleapis.com/youtube/v3/playlistItems?key=${youtube_api_key}&playlistId=${playlistId}&part=snippet", null,
+//                    { response ->
+//                        val items : JSONArray = response.getJSONArray("items")
+//
+//                        val videoTitles = arrayOfNulls<String>(items.length())
+//                        for (i in 0 until items.length()) {
+//                            val snippet : JSONObject = items.getJSONObject(i).getJSONObject("snippet")
+//                            val title : String = snippet.getString("title")
+//                            videoTitles[i] = title
 //                        }
-                        for (i in 0 until items.length()) {
-                            val snippet : JSONObject = items.getJSONObject(i).getJSONObject("snippet")
-                            val title : String = snippet.getString("title")
-                            videoTitles[i] = title
-                        }
-
-                        val adapter = ArrayAdapter(
-                            this,
-                            android.R.layout.simple_list_item_1, videoTitles
-                        )
-                        listView.adapter = adapter
-
-                        listView.setOnItemClickListener { _, _, position, _ ->
-                            val selectedVideoSnippet = items.getJSONObject(position).getJSONObject("snippet")
-                            val videoId: String =  selectedVideoSnippet.getJSONObject("resourceId").getString("videoId")
-                            val title: String =  selectedVideoSnippet.getString("title")
-
-//                            val selectedVideo: Video = Video(selectedVideoSnippet.getString("videoId"), selectedVideoSnippet.getString("title"))
-                            val videoIntent = VideoActivity.newIntent(this, videoId)
-//                            val videoIntent = VideoActivity.newIntent(this, VideoActivity).apply {
-//                                putExtra("title", selectedVideo.getString("title"))
-//                            }
-                            startActivity(videoIntent)
-                        }
-                    },
-                    { textView.text = "That didn't work!" })
-
-                queue.add(jsonObjectRequestVideos)
-
-            },
-            { textView.text = "That didn't work!" })
-
-        queue.add(jsonObjectRequest)
+//
+//                        val adapter = ArrayAdapter(
+//                            this,
+//                            android.R.layout.simple_list_item_1, videoTitles
+//                        )
+//                        listView.adapter = adapter
+//
+//                        listView.setOnItemClickListener { _, _, position, _ ->
+//                            val selectedVideoSnippet = items.getJSONObject(position).getJSONObject("snippet")
+//                            val videoId: String =  selectedVideoSnippet.getJSONObject("resourceId").getString("videoId")
+//                            val title: String =  selectedVideoSnippet.getString("title")
+//
+//                            val videoIntent = VideoActivity.newIntent(this, videoId)
+//                            startActivity(videoIntent)
+//                        }
+//                    },
+//                    { textView.text = "That didn't work!" })
+//
+//                queue.add(jsonObjectRequestVideos)
+//            },
+//            { textView.text = "That didn't work!" })
+//
+//        queue.add(jsonObjectRequest)
     }
 }
